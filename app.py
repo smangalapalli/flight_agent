56b7ce2df317864ac359a0a032e10227d4e6630a
smangalapalli <sandhya.mangalapalli@gmail.com>
2026-03-29 11:09:45 -0700

    second commit

--- app.py
+++ app.py
@@ -150,7 +150,8 @@ with gr.Blocks(title="✈️ AI Flight Search") as demo:
 
 
 if __name__ == "__main__":
-    demo.launch()
-        #server_name="0.0.0.0",
-        #server_port=int(os.environ.get("PORT", 7860))
-    
+    #demo.launch()
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
